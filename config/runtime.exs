dd48b49ac849c0d10567ebc9ffa7cb99bc3845df
Wahaj Rashid <wahaj1020@gmail.com>
2025-07-29 08:14:32 +0100

    updated3

--- config/runtime.exs
+++ config/runtime.exs
@@ -42,7 +42,7 @@ if config_env() == :prod do
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
-  config :calorie_ai, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
+  config :calorie_ai, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY") || 121
 
   config :calorie_ai, CalorieAiWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
