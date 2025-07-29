ca6da59407de25f3b9484a700e46cabdc887c13e
Wahaj Rashid <wahaj1020@gmail.com>
2025-07-29 15:33:22 +0100

    latest5

--- lib/calorie_ai_web/endpoint.ex
+++ lib/calorie_ai_web/endpoint.ex
@@ -3,7 +3,7 @@ defmodule CalorieAiWeb.Endpoint do
 
 
   plug CORSPlug,
-  origin: ["https://frontend-ai-calorieapp-wahajrashid1020s-projects.vercel.app"],
+  origin: ["https://mymealapp.vercel.app/"],
   methods: ["GET", "POST", "OPTIONS"],
   headers: ["Content-Type", "Authorization"]
 
