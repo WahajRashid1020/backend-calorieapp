c3842297fdd0d4618afbbfe5fda36df700f29600
Wahaj Rashid <wahaj1020@gmail.com>
2025-07-29 08:21:42 +0100

    no sec

--- config/runtime.exs
+++ config/runtime.exs
@@ -17,38 +17,15 @@ if System.get_env("PHX_SERVER") do
   config :calorie_ai, CalorieAiWeb.Endpoint, server: true
 end
 
+# Production-specific settings (if needed)
 if config_env() == :prod do
-  database_url = "ecto://postgres:postgres@localhost/dummy" ||
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
-
-  maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
-
-  config :calorie_ai, CalorieAi.Repo,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
-
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") || 10
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
-
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
-
-  config :calorie_ai, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY") || 121
 
   config :calorie_ai, CalorieAiWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
-    ],
-    secret_key_base: secret_key_base
+    ]
 end
